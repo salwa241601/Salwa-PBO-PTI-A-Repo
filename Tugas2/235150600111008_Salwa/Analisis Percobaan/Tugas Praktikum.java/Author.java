027438c710b393e96230a55c0b659a9b0e7305f8
salwa241601 <160576310+salwa241601@users.noreply.github.com>
2024-03-13 22:35:22 +0700

    Create Author.java

---
+++ Tugas2/235150600111008_Salwa/Analisis Percobaan/Tugas Praktikum.java/Author.java
@@ -0,0 +1,12 @@
+public class Author {
+    private String nama;
+
+    public Author(String nama){
+        this.nama = nama;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+}
